8f1fd639425343e7646ee98964f34a274b0752ee
Felipe Pouysségur <pipemdp@hotmail.com>
2022-10-21 15:10:58 -0300

    preentrega Pouyssegur

---
+++ src/app/components/CartWidget.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Imagen from '../assets/img/cart.png'
+
+function CartWidget() {
+  return (
+    <div className='logo-carrito'>
+    <img src={Imagen} />
+    </div>
+  )
+}
+
+export default CartWidget
---
+++ src/app/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ItemListContainer = ({greetings}) => {
+  return (
+    <div>
+      <h2 className='bienvenida'>{greetings}</h2>
+    </div>
+    
+  )
+}
+
+export default ItemListContainer
