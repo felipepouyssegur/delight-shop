3ccad7c565d3be0ec881bf503c4d25eead8cc3dc
Felipe Pouysségur <pipemdp@hotmail.com>
2022-10-25 11:22:04 -0300

    subida

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import NavBar from './app/components/NavBar'
 import './app/styles.css'
 
 const App = () => {
+let fecha = new Date()
 const mensaje = "Bienvenido a DELIGHT"
-
   return (
     <div>
      <NavBar/>
      <ItemListContainer greetings = {mensaje}></ItemListContainer>
+     <p>{fecha}</p>
     </div>
   );
 }
